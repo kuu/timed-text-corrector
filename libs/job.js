15014195e37c7a73246192d799ca40809be7e427
Kuu Miyazaki <miyazaqui@gmail.com>
2017-10-09 14:55:18 +0900

    Update

--- libs/job.js
+++ libs/job.js
@@ -2,22 +2,33 @@ const TTML = require('ttml');
 const debug = require('debug')('ttc');
 const timedtext = require('../models/timedtext');
 const transcript = require('../models/transcript');
-const dummyTTObj = require('./dummy-timedtext.json');
-const dummyTranscriptStr = require('./dummy-transcript.json');
 
-transcript.add('05dTl3YzE6gkKPU35hdAIpI3WwszlHzm', dummyTranscriptStr);
+transcript.add('05dTl3YzE6gkKPU35hdAIpI3WwszlHzm', require('./dummy-transcript.json'));
+transcript.add('05dTl3YzE6gkKPU35hdAIpI3WwszlHzm', require('./dummy-transcript-02.json'));
+
+const ttObjList = {};
+ttObjList['05dTl3YzE6gkKPU35hdAIpI3WwszlHzm'] = require('./dummy-timedtext.json');
+ttObjList['lxYjN5YzE6uh_xH3qoshLPi23u8rWOSc'] = require('./dummy-timedtext-02.json');
 
 function startJob(jobId) {
-  setTimeout(() => {
-    const ttml = TTML.stringify(dummyTTObj);
-    timedtext.update(jobId, 'processed', ttml)
-    .then(() => {
-      debug(`Succeeded to update job: ${jobId}`);
-    })
-    .catch(err => {
-      debug(`Failed to update job: ${err.stack}`);
-    });
-  }, 80000);
+  timedtext.find(jobId)
+  .then(data => {
+    const assetId = data.assetId;
+    const dummyTTObj = ttObjList[assetId];
+    if (!dummyTTObj) {
+      throw new Error('No dummy timedtext object!');
+    }
+    setTimeout(() => {
+      const ttml = TTML.stringify(dummyTTObj);
+      timedtext.update(jobId, 'processed', ttml)
+      .then(() => {
+        debug(`Succeeded to update job: ${jobId}`);
+      })
+      .catch(err => {
+        debug(`Failed to update job: ${err.stack}`);
+      });
+    }, 70000);
+  });
 }
 
 module.exports = {
--- libs/util.js
+++ libs/util.js
@@ -10,7 +10,38 @@ function formalizeTimedText() {
   return [];
 }
 
+function formatAzureVideoIndexer(srcData, language) {
+  if (!Array.isArray(srcData)) {
+    throw new TypeError('Invalid data format: Azure Video Indexer');
+  }
+  const destLines = [];
+  for (const {lines} of srcData) {
+    if (!lines || !lines.length) {
+      continue;
+    }
+    for (const srcObj of lines) {
+      const timeRange = srcObj.timeRange || srcObj.adjustedTimeRange;
+      if (!timeRange || !timeRange.start || !timeRange.end) {
+        continue;
+      }
+      destLines.push({
+        start: timeRange.start,
+        end: timeRange.end,
+        text: srcObj.text
+      });
+    }
+  }
+  const destObj = {
+    languages: {}
+  };
+  destObj.languages[language] = {
+    lines: destLines
+  };
+  return destObj;
+}
+
 module.exports = {
   formalizeTranscript,
-  formalizeTimedText
+  formalizeTimedText,
+  formatAzureVideoIndexer
 };
